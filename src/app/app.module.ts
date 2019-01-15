9aab3c150d858f2f745d79afce38477550fd763e
thienn <thien.oslo@gmail.com>
2019-01-15 06:18:58 +0100

    Connection to Firebase base setup
    + Set up of firebase + angularfire.
    + Env file with the configs hidden
    + Base setup of Subscription model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+ import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { environment } from '../environments/env';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseconfig), // File not included in the git repo for security reasons
+    AngularFireDatabase
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/subscription.model.ts
@@ -0,0 +1,9 @@
+export class Subscription {
+    id: string;
+    title: string;
+    amount: number;
+    description: string;
+    paydate: string;
+    startdate: Date;
+    enddate: Date;
+}
